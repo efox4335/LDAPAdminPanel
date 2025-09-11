3c36848144adf8a7b7d2889097a443ad5cdd1e58
emma <emma.t.christy@gmail.com>
2025-09-11 14:52:59 +0300

    added endpoint for setting custom ca certs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 
+import settingsRouter from './routes/settings';
+
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/settings', settingsRouter);
 
 app.listen(PORT, () => {
   console.log(`backend up at port ${PORT}`);
---
+++ backend/src/routes/settings.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import * as z from 'zod';
+
+import { setCaCerts } from '../utils/config';
+import { stringArraySchema } from '../utils/scheams';
+
+const router = express.Router();
+
+router.post('caCerts', (req, rsp) => {
+  try {
+    const newCerts = stringArraySchema.parse(req.body);
+
+    setCaCerts(newCerts);
+
+    rsp.send(newCerts).status(201);
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      rsp.send({ error: 'invalid certs' }).status(400);
+    }
+  }
+});
+
+export default router;
