6a0fc5b011e3d0d69aca042963c9e95be4ac9ada
emma <emma.t.christy@gmail.com>
2025-09-16 10:27:13 +0300

    added correct url test

--- backend/tests/routes/ldapdb.test.ts
+++ backend/tests/routes/ldapdb.test.ts
@@ -7,8 +7,17 @@ import app from '../../src/app';
 
 describe('client endpoint tests', (): void => {
   describe('ldap dp url tests', () => {
+    test('correct url', async () => {
+      await supertest(app)
+        .post('/ldapdb/client')
+        .send({ url: 'ldap://localhost:1389' })
+        .expect(201);
+    });
     test('object passed', async () => {
-      await supertest(app).post('/ldapdb/client').send({ str: 'abcdef' }).expect(400);
+      await supertest(app)
+        .post('/ldapdb/client')
+        .send({ str: 'abcdef' })
+        .expect(400);
     });
   });
 });
