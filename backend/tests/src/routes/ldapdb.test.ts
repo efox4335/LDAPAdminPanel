d0de1f1f4a0652e2a165165ef8f42de23005e4f7
emma <emma.t.christy@gmail.com>
2025-09-17 08:50:01 +0300

    updated new client tests

--- backend/tests/src/routes/ldapdb.test.ts
+++ backend/tests/src/routes/ldapdb.test.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import test from 'node:test';
 import { describe } from 'node:test';
 import supertest from 'supertest';
+import expect from 'expect';
 
 import app from '../../../src/app';
 import { serverUrl } from '../../testUtils';
@@ -10,16 +13,27 @@ describe('ldapdbs endpoint tests', () => {
   describe('new client tests', (): void => {
     describe('ldap dp url tests', () => {
       test('correct url', async () => {
-        await supertest(app)
+        const rsp = await supertest(app)
           .post('/ldapdbs/')
           .send({ url: serverUrl })
           .expect(201);
+
+        expect(rsp.body.id).toBeDefined();
+        expect(typeof (rsp.body.id)).toStrictEqual('string');
+
+        //required to not pollute server with unused client
+        await supertest(app)
+          .delete(`/ldapbds/${rsp.body.id}`);
       });
       test('object passed', async () => {
-        await supertest(app)
+        const rsp = await supertest(app)
           .post('/ldapdbs/')
           .send({ str: 'abcdef' })
           .expect(400);
+
+        expect(rsp.body.error).toBeDefined();
+        expect(typeof (rsp.body.error)).toStrictEqual('string');
+        expect(rsp.body.error).toStrictEqual('url is invalid');
       });
     });
   });
