8c612df1876a514768b28cb78f540951d9dab88d
emma <emma.t.christy@gmail.com>
2025-09-17 08:24:31 +0300

    fixed new client test

--- backend/tests/routes/ldapdb.test.ts
+++ backend/tests/routes/ldapdb.test.ts
@@ -5,17 +5,17 @@ import supertest from 'supertest';
 
 import app from '../../src/app';
 
-describe('client endpoint tests', (): void => {
+describe('new client tests', (): void => {
   describe('ldap dp url tests', () => {
     test('correct url', async () => {
       await supertest(app)
-        .post('/ldapdb/client')
+        .post('/ldapdbs/')
         .send({ url: 'ldap://localhost:1389' })
         .expect(201);
     });
     test('object passed', async () => {
       await supertest(app)
-        .post('/ldapdb/client')
+        .post('/ldapdbs/')
         .send({ str: 'abcdef' })
         .expect(400);
     });
