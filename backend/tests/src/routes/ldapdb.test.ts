345f47ac48c0ed2d073d62072bc919148e68d34d
emma <emma.t.christy@gmail.com>
2025-09-17 08:33:58 +0300

    added outer describe for ldapdbs endpoint tests

--- backend/tests/src/routes/ldapdb.test.ts
+++ backend/tests/src/routes/ldapdb.test.ts
@@ -6,19 +6,21 @@ import supertest from 'supertest';
 import app from '../../../src/app';
 import { serverUrl } from '../../testUtils';
 
-describe('new client tests', (): void => {
-  describe('ldap dp url tests', () => {
-    test('correct url', async () => {
-      await supertest(app)
-        .post('/ldapdbs/')
-        .send({ url: serverUrl })
-        .expect(201);
-    });
-    test('object passed', async () => {
-      await supertest(app)
-        .post('/ldapdbs/')
-        .send({ str: 'abcdef' })
-        .expect(400);
+describe('ldapdbs endpoint tests', () => {
+  describe('new client tests', (): void => {
+    describe('ldap dp url tests', () => {
+      test('correct url', async () => {
+        await supertest(app)
+          .post('/ldapdbs/')
+          .send({ url: serverUrl })
+          .expect(201);
+      });
+      test('object passed', async () => {
+        await supertest(app)
+          .post('/ldapdbs/')
+          .send({ str: 'abcdef' })
+          .expect(400);
+      });
     });
   });
 });
