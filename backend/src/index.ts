1384f8320ec8e09f9ac51fa1db479d1f23ee6e5e
emma <emma.t.christy@gmail.com>
2025-09-18 09:31:12 +0300

    moved backend up message to logging function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import app from './app';
+import writeToLog from './utils/logger';
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`backend up at port ${PORT}`);
+  writeToLog(`backend up at port ${PORT}`);
 });
