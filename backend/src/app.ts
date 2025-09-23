6165130455ccd355e1279d4a9b4abb0eb68e0bd6
emma <emma.t.christy@gmail.com>
2025-09-23 13:17:50 +0300

    enabled error handling middleware for ldap errors and added test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import ldapDbRouter from './routes/ldapdb';
 import { loggerMiddleware } from './middlewares/loggers';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(express.json());
 app.use(loggerMiddleware);
 
 app.use('/ldapdbs', ldapDbRouter);
+
+app.use(errorHandler);
 
 export default app;
---
+++ backend/tests/src/middlewares/errorHandler.test.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import test, { afterEach, beforeEach } from 'node:test';
+import { describe } from 'node:test';
+import supertest from 'supertest';
+import expect from 'expect';
+
+import app from '../../../src/app';
+import { testClients } from '../../testUtils';
+
+describe('error handler tests', () => {
+  const clients = new testClients;
+
+  beforeEach(async () => {
+    await clients.addClients(app);
+    await clients.bindClients(app);
+  });
+
+  afterEach(async () => {
+    await clients.unbindClients(app);
+    await clients.delClients(app);
+  });
+
+  test('ldap error', async () => {
+    const res = await supertest(app)
+      .delete(`/ldapdbs/${clients.adminClient}/del`)
+      .send({ dn: 'dc=invalidDn' })
+      .expect(400);
+
+    expect(res.body).toBeDefined();
+    expect(res.body).toStrictEqual({ type: 'ldapError', code: 53, name: 'UnwillingToPerformError' });
+  });
+});
