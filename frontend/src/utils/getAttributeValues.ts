d6d69c6074f52149c0d17557a60e66c97dbf8e86
emma <emma.t.christy@gmail.com>
2025-10-08 10:23:37 +0300

    fixed getAttributeValues not handeling some whitespace

--- frontend/src/utils/getAttributeValues.ts
+++ frontend/src/utils/getAttributeValues.ts
@@ -1,5 +1,12 @@
 const getAttributeValues = (values: string): string[] => {
-  return values.split(/\s*,\s*/).filter((val) => val !== '');
+  return values
+    .split(/\s*,\s*/)
+    .map((val) => {
+      const startWhiteSpaceRemoved = val.replace(/^\s*/g, '');
+
+      return startWhiteSpaceRemoved.replace(/\s*$/g, '');
+    })
+    .filter((val) => val !== '');
 };
 
 export default (getAttributeValues);
--- frontend/tests/src/utils/getAttributeValues.test.ts
+++ frontend/tests/src/utils/getAttributeValues.test.ts
@@ -10,4 +10,44 @@ describe('getAttributeValues.ts tests', () => {
 
     expect(res).toStrictEqual(['123', '456', '789', '10', '11']);
   });
+
+  test('arr with whitespace', () => {
+    const data = '  1, 2  , 3  ';
+
+    const res = getAttributeValues(data);
+
+    expect(res).toStrictEqual(['1', '2', '3']);
+  });
+
+  test('single value with whitespace', () => {
+    const leadingWhitespace = '   1';
+
+    const noLeadingWhitespace = getAttributeValues(leadingWhitespace);
+
+    expect(noLeadingWhitespace).toStrictEqual(['1']);
+
+    const trailingWhitespace = '1   ';
+
+    const noTrailingWhitespace = getAttributeValues(trailingWhitespace);
+
+    expect(noTrailingWhitespace).toStrictEqual(['1']);
+
+    const bothWhitespace = '  1  ';
+
+    const noWhitespace = getAttributeValues(bothWhitespace);
+
+    expect(noWhitespace).toStrictEqual(['1']);
+  });
+
+  test('just whitespace', () => {
+    const justWhitespaceArr = '  ,   ,, ';
+
+    const emptyArr = getAttributeValues(justWhitespaceArr);
+
+    expect(emptyArr).toStrictEqual([]);
+
+    const whitespaceVal = '   ';
+
+    expect(getAttributeValues(whitespaceVal)).toStrictEqual([]);
+  });
 });
