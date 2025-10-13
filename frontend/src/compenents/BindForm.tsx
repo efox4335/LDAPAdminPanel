02bfe1bbeb1cd353bbbead6682a0f249c33a634d
emma <emma.t.christy@gmail.com>
2025-10-13 09:24:56 +0300

    added support for controls to bind

---
+++ frontend/src/compenents/BindForm.tsx
@@ -0,0 +1,65 @@
+import { useState, type SyntheticEvent } from 'react';
+import { useDispatch } from 'react-redux';
+
+import type { bindReq, client, newControlObject } from '../utils/types';
+import { bindClient } from '../services/ldapdbsService';
+import { addClient } from '../slices/client';
+import { addError } from '../slices/error';
+import NewLdapControls from './NewLdapControls';
+import getControls from '../utils/getControls';
+
+const BindForm = ({ client }: { client: client }) => {
+  const [newDn, setNewDn] = useState<string>('');
+  const [newPassword, setNewPassword] = useState<string>('');
+  const [newControls, setNewControls] = useState<newControlObject[]>([]);
+
+  const dispatch = useDispatch();
+
+  const handleBind = async (event: SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const req: bindReq = {
+        dnOrSaslMechanism: newDn,
+        password: (newPassword === '') ? undefined : newPassword,
+        control: getControls(newControls)
+      };
+
+      setNewDn('');
+      setNewPassword('');
+      setNewControls([]);
+
+      await bindClient(client.id, req);
+
+      const newClient = {
+        ...client,
+        isConnected: true,
+        boundDn: req.dnOrSaslMechanism
+      };
+
+      dispatch(addClient(newClient));
+    } catch (err) {
+      dispatch(addError(err));
+    }
+  };
+
+  return (
+    <div>
+      <h4>bind: </h4>
+      <form onSubmit={handleBind}>
+        Dn:
+        <input value={newDn} onChange={(event) => setNewDn(event.target.value)} />
+        <br></br>
+        password:
+        <input type='password' value={newPassword} onChange={(event) => setNewPassword(event.target.value)} />
+        <button>bind</button>
+        <br></br>
+        controls:
+        <br></br>
+        <NewLdapControls newControls={newControls} setNewControls={setNewControls} />
+      </form>
+    </div>
+  );
+};
+
+export default BindForm;
--- frontend/src/compenents/SingleClient.tsx
+++ frontend/src/compenents/SingleClient.tsx
@@ -1,18 +1,15 @@
-import { useState, type SyntheticEvent } from 'react';
 import { useAppDispatch as useDispatch } from '../utils/reduxHooks';
 
-import type { bindReq, client } from '../utils/types';
-import { deleteClient, bindClient, unbindClient } from '../services/ldapdbsService';
+import type { client } from '../utils/types';
+import { deleteClient, unbindClient } from '../services/ldapdbsService';
 import { delClient, addClient } from '../slices/client';
 import { addError } from '../slices/error';
 import LdapTree from './LdapTree';
 import generateLdapServerTree from '../utils/generateLdapServerTree';
 import { fetchAllLdapEntries } from '../utils/query';
+import BindForm from './BindForm';
 
 const SingleClient = ({ client }: { client: client }) => {
-  const [newDn, setNewDn] = useState<string>('');
-  const [newPassword, setNewPassword] = useState<string>('');
-
   const dispatch = useDispatch();
 
   const boundDn = (client.boundDn === null) ? 'null' : client.boundDn;
@@ -24,32 +21,6 @@ const SingleClient = ({ client }: { client: client }) => {
       await deleteClient(client.id);
 
       dispatch(delClient(client.id));
-    } catch (err) {
-      dispatch(addError(err));
-    }
-  };
-
-  const handleBind = async (event: SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    try {
-      const req: bindReq = {
-        dnOrSaslMechanism: newDn,
-        password: (newPassword === '') ? undefined : newPassword
-      };
-
-      setNewDn('');
-      setNewPassword('');
-
-      await bindClient(client.id, req);
-
-      const newClient = {
-        ...client,
-        isConnected: true,
-        boundDn: req.dnOrSaslMechanism
-      };
-
-      dispatch(addClient(newClient));
     } catch (err) {
       dispatch(addError(err));
     }
@@ -101,15 +72,7 @@ const SingleClient = ({ client }: { client: client }) => {
       <br></br>
       {connectionString}
       <br></br>
-      <h4>bind: </h4>
-      <form onSubmit={handleBind}>
-        Dn:
-        <input value={newDn} onChange={(event) => setNewDn(event.target.value)} />
-        <br></br>
-        password:
-        <input type='password' value={newPassword} onChange={(event) => setNewPassword(event.target.value)} />
-        <button>bind</button>
-      </form>
+      <BindForm client={client} />
       <br></br>
       <button onClick={handleUnbind}>unbind</button>
       <button onClick={handleDelete}>remove</button>
