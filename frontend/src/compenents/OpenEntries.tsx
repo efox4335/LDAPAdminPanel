91a4dc55c4b0913942864072b60713e6f78f871d
emma <emma.t.christy@gmail.com>
2025-10-16 12:55:38 +0300

    added updated entries css

--- frontend/src/compenents/OpenEntries.tsx
+++ frontend/src/compenents/OpenEntries.tsx
@@ -7,17 +7,19 @@ const OpenEntries = ({ clientId }: { clientId: string }) => {
   const openEntries = useSelector(((state) => selectOpenEntriesByClientId(state, clientId)));
 
   return (
-    <div className='openEntries'>
-      <div className='openEntriesHeader'>
-        <h4>Open entries</h4>
+    <div className='openEntriesContainer'>
+      <h4 className='openEntriesHeader'>
+        open entries
+      </h4>
+      <div className='openEntries'>
+        {openEntries.map((entry) => {
+          return (
+            <div key={entry.dn} className='openEntryContainer'>
+              <SingleOpenEntry clientId={clientId} entry={entry} />
+            </div>
+          );
+        })}
       </div>
-      {openEntries.map((entry) => {
-        return (
-          <div key={entry.dn} className='openEntriesContainer'>
-            <SingleOpenEntry clientId={clientId} entry={entry} />
-          </div>
-        );
-      })}
     </div>
   );
 };
