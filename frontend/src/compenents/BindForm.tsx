a5a97963df79a0b51e3103373d7e3017457647e8
emma <emma.t.christy@gmail.com>
2025-10-28 09:09:18 +0200

    updated BindForm style

--- frontend/src/compenents/BindForm.tsx
+++ frontend/src/compenents/BindForm.tsx
@@ -49,14 +49,29 @@ const BindForm = ({ client }: { client: client }) => {
 
   return (
     <div className='singleClientBind'>
-      <h4>bind: </h4>
+      <h4>bind</h4>
       <form onSubmit={handleBind} className='singleClientBindForm'>
         <div className='userInteractionContainer'>
-          Dn:
-          <input value={newDn} onChange={(event) => setNewDn(event.target.value)} />
-          <br></br>
-          password:
-          <input type='password' value={newPassword} onChange={(event) => setNewPassword(event.target.value)} />
+          <table>
+            <tbody>
+              <tr className='headlessFirstTableRow'>
+                <td>
+                  dn
+                </td>
+                <td>
+                  <input value={newDn} onChange={(event) => setNewDn(event.target.value)} />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  password
+                </td>
+                <td>
+                  <input type='password' value={newPassword} onChange={(event) => setNewPassword(event.target.value)} />
+                </td>
+              </tr>
+            </tbody>
+          </table>
           <br></br>
           <NewLdapControls tableName='controls' newControls={newControls} setNewControls={setNewControls} />
         </div>
