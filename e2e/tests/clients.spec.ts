5aef2a8f3ca6b6fae19f2cb656c40c25ee285282
emma <emma.t.christy@gmail.com>
2025-10-30 08:13:19 +0200

    added client tests

---
+++ e2e/tests/clients.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+import { ldapServerUrl } from '../utils/constants';
+
+test.describe('client tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173');
+  });
+
+  test('incorrect protocol causes error', async ({ page }) => {
+    await page.getByRole('textbox').fill('invalidProtocol');
+
+    await page.getByRole('button', { name: 'add' }).click();
+
+    await expect(page.getByText('is an invalid LDAP URL')).toBeVisible();
+  });
+
+  test('reset works', async ({ page }) => {
+    await page.getByRole('textbox').fill('stuff');
+
+    await page.getByRole('button', { name: 'reset' }).click();
+
+    await expect(page.getByRole('textbox')).toHaveText('');
+  });
+
+  test('correct add and remove', async ({ page }) => {
+    await page.getByRole('textbox').fill(ldapServerUrl);
+
+    await page.getByRole('button', { name: 'add' }).click();
+
+    await expect(page.getByText(ldapServerUrl)).toBeVisible();
+
+    await page.getByRole('button', { name: 'remove' }).click();
+
+    await expect(page.getByText(ldapServerUrl)).toBeHidden();
+  });
+});
