410b5fedd213554283f433697e143477f4893ea1
emma <emma.t.christy@gmail.com>
2026-01-06 10:29:00 +0200

    added admin bind and unbind utils

--- e2e/utils/preTestUtils.ts
+++ e2e/utils/preTestUtils.ts
@@ -1,6 +1,6 @@
 import type { Page } from '@playwright/test'
 
-import { pageUrl, ldapServerUrl } from './constants';
+import { pageUrl, ldapServerUrl, adminDn, adminPassword } from './constants';
 
 export const addServer = async (page: Page) => {
   await page.goto(pageUrl);
@@ -12,4 +12,29 @@ export const addServer = async (page: Page) => {
 
 export const removeServer = async (page: Page) => {
   await page.getByRole('button', { name: 'remove' }).click();
+}
+
+export const adminBind = async (page: Page) => {
+  await page
+    .locator('.singleClientBind')
+    .getByText('dn')
+    .locator('..')
+    .getByRole('textbox')
+    .fill(adminDn);
+
+  await page
+    .locator('.singleClientBind')
+    .getByText('password')
+    .locator('..')
+    .getByRole('textbox')
+    .fill(adminPassword);
+
+  await page
+    .locator('.singleClientBind')
+    .getByRole('button', { name: 'bind' })
+    .click();
+}
+
+export const unbind = async (page: Page) => {
+  await page.getByRole('button', { name: 'unbind' }).click();
 }
