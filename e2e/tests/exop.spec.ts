2be63a1f589bc6b2133389b535d319cad4684ca5
emma <emma.t.christy@gmail.com>
2026-01-06 11:30:16 +0200

    added exop tests and utils

---
+++ e2e/tests/exop.spec.ts
@@ -0,0 +1,109 @@
+import { test, expect } from '@playwright/test';
+
+import { addServer, removeServer, adminBind, unbind } from '../utils/preTestUtils';
+import { whoAmIOid, invalidOid } from '../utils/constants';
+import assertError from '../utils/assertError';
+
+test.describe('exop tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await addServer(page);
+    await adminBind(page);
+  })
+
+  test.afterEach(async ({ page }) => {
+    await unbind(page);
+    await removeServer(page);
+  })
+
+  test('success', async ({ page }) => {
+    await page
+      .locator('.singleClientExopForm')
+      .locator('.userInteractionContainer')
+      .getByRole('textbox')
+      .first()
+      .fill(whoAmIOid);
+
+    await page
+      .locator('.singleClientExop')
+      .getByRole('button', { name: 'start' })
+      .click();
+
+    await expect(
+      page
+        .locator('.singleClientExop')
+        .locator('.userInteractionContainer')
+        .getByText('result')
+        .locator('..')
+        .getByRole('cell')
+        .nth(1)
+    ).toHaveText(RegExp('.*success.*'));
+  });
+
+  test('error on fail', async ({ page }) => {
+    await page
+      .locator('.singleClientExopForm')
+      .locator('.userInteractionContainer')
+      .getByRole('textbox')
+      .first()
+      .fill(invalidOid);
+
+    await page
+      .locator('.singleClientExop')
+      .getByRole('button', { name: 'start' })
+      .click();
+
+    await expect(
+      page
+        .locator('.singleClientExop')
+        .locator('.userInteractionContainer')
+        .getByText('result')
+        .locator('..')
+        .getByRole('cell')
+        .nth(1)
+    ).toHaveText(RegExp('.*fail.*'));
+
+    await assertError(page, 'ProtocolError', true);
+  });
+
+  test('control passed', async ({ page }) => {
+    await page
+      .locator('.singleClientExopForm')
+      .locator('.userInteractionContainer')
+      .getByRole('textbox')
+      .first()
+      .fill(whoAmIOid);
+
+    const controlForm = page
+      .locator('.singleClientExopForm')
+      .getByText('controls')
+      .locator('..')
+      .locator('..')
+      .locator('..');
+
+    await controlForm
+      .getByRole('textbox')
+      .fill(invalidOid);
+
+    await controlForm
+      .getByRole('checkbox')
+      .nth(0)
+      .check();
+
+    await page
+      .locator('.singleClientExop')
+      .getByRole('button', { name: 'start' })
+      .click();
+
+    await expect(
+      page
+        .locator('.singleClientExop')
+        .locator('.userInteractionContainer')
+        .getByText('result')
+        .locator('..')
+        .getByRole('cell')
+        .nth(1)
+    ).toHaveText(RegExp('.*fail.*'));
+
+    await assertError(page, 'UnavailableCriticalExtensionError', true);
+  });
+});
--- e2e/utils/constants.ts
+++ e2e/utils/constants.ts
@@ -7,3 +7,5 @@ export const adminDn: string = 'cn=admin,dc=example,dc=org';
 export const adminPassword: string = 'password';
 
 export const invalidOid: string = '1.2';
+
+export const whoAmIOid: string = '1.3.6.1.4.1.4203.1.11.3';
