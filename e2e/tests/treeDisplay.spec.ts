77d36fce5198713b6c464a20d2e7d25dc2cb1086
emma <emma.t.christy@gmail.com>
2026-01-08 12:25:25 +0200

    added tree display tests

---
+++ e2e/tests/treeDisplay.spec.ts
@@ -0,0 +1,42 @@
+import { test } from '@playwright/test';
+
+import { addServer, removeServer, adminBind, unbind } from '../utils/preTestUtils';
+import { locateEntry, defaultTreeEntries, collapseEntry, expandEntry } from '../utils/treeDisplayUtils';
+
+test.describe('ldap tree display tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await addServer(page);
+
+    await adminBind(page);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await unbind(page);
+
+    await removeServer(page);
+  });
+
+  test('default tree visibility', async ({ page }) => {
+    for (const entryDn of defaultTreeEntries) {
+      await locateEntry(page, entryDn, true);
+    }
+  });
+
+  test('entry can collapse', async ({ page }) => {
+    await collapseEntry(page, 'dc=example,dc=org');
+
+    try {
+      await locateEntry(page, 'cn=user01,ou=users,dc=example,dc=org');
+
+      throw new Error('entry was visible');
+    } catch { /* if there was an error then the child entry was hidden */ };
+  });
+
+  test('entry can expand', async ({ page }) => {
+    await collapseEntry(page, 'dc=example,dc=org');
+
+    await expandEntry(page, 'dc=example,dc=org');
+
+    await locateEntry(page, 'cn=user01,ou=users,dc=example,dc=org');
+  });
+});
---
+++ e2e/utils/treeDisplayUtils.ts
@@ -0,0 +1,85 @@
+import { type Page, type Locator, expect } from '@playwright/test';
+
+const locateTreeDisplay = (page: Page): Locator => {
+  return page.locator('.ldapTreeDisplayContainer');
+};
+
+export const locateEntry = async (page: Page, distinguishedName: string, expandEntries?: boolean): Promise<Locator> => {
+  expandEntries = expandEntries || false;
+
+  const treeDisplay = locateTreeDisplay(page);
+
+  const commonNames = distinguishedName.split(',').reverse();
+
+  let curLocation = treeDisplay
+    .locator('.userInteractionContainer')
+    .locator('.ldapTreeEntry')
+    .first();
+
+  await expect(curLocation).toBeVisible();
+
+  let curCommonName = '';
+
+  for (const commonName of commonNames) {
+    if (expandEntries) {
+      const curVisButton = curLocation.locator('.ldapEntryVisibilityToggle').first().getByText('+');
+
+      if (await curVisButton.isVisible()) {
+        await curVisButton.click();
+      }
+    }
+
+    if (curCommonName.length > 0) {
+      curCommonName = commonName + ',' + curCommonName;
+    } else {
+      curCommonName = commonName;
+    }
+
+    const nextLocation = curLocation
+      .locator('.ldapTreeEntryChildren>*')
+      .filter({ has: page.getByText(new RegExp(`^${curCommonName}\$`)) });
+
+    try {
+      await expect(nextLocation).toBeVisible({ timeout: 100 });
+
+      curLocation = nextLocation.locator('..');
+
+      curCommonName = '';
+    } catch { }
+  }
+
+  if (curCommonName !== '') {
+    throw `no entry for ${distinguishedName} located`;
+  }
+
+  return curLocation;
+};
+
+export const collapseEntry = async (page: Page, distinguishedName: string) => {
+  const curEntry = await locateEntry(page, distinguishedName);
+
+  const displayToggleButton = curEntry.locator('.ldapEntryVisibilityToggle').first().getByText('-');
+
+  if (await displayToggleButton.isVisible()) {
+    await displayToggleButton.click();
+  }
+};
+
+export const expandEntry = async (page: Page, distinguishedName: string) => {
+  const curEntry = await locateEntry(page, distinguishedName);
+
+  const displayToggleButton = curEntry.locator('.ldapEntryVisibilityToggle').first().getByText('+');
+
+  if (await displayToggleButton.isVisible()) {
+    await displayToggleButton.click();
+  }
+};
+
+export const defaultTreeEntries = [
+  'dc=example,dc=org',
+  'ou=users,dc=example,dc=org',
+  'ou=groups,dc=example,dc=org',
+  'cn=user01,ou=users,dc=example,dc=org',
+  'cn=user02,ou=users,dc=example,dc=org',
+  'cn=readers,ou=groups,dc=example,dc=org',
+];
