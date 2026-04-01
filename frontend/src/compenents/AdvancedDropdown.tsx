bf4beb8fbf63abf6745167e52c4e43e3bb627214
emma <emma.t.christy@gmail.com>
2026-04-01 11:54:50 +0300

    added dropDownState to TextWrapper

--- frontend/src/compenents/AdvancedDropdown.tsx
+++ frontend/src/compenents/AdvancedDropdown.tsx
@@ -1,13 +1,22 @@
 import { useState, type ReactNode, type ComponentType, type JSX } from 'react';
 
 const AdvancedDropdown =
-  <T extends { DropDownButton: JSX.Element, displayText: string } = { DropDownButton: JSX.Element, displayText: string }>
+  <T extends {
+    DropDownButton: JSX.Element,
+    displayText: string,
+    dropDownState: boolean
+  } =
+    {
+      DropDownButton: JSX.Element,
+      displayText: string
+      dropDownState: boolean
+    }>
     ({ children, displayText, TextWrapper, wrapperProps }:
       {
         children: ReactNode,
         displayText: string,
         TextWrapper?: ComponentType<T>,
-        wrapperProps?: Omit<T, 'displayText' | 'DropDownButton'>
+        wrapperProps?: Omit<T, 'displayText' | 'DropDownButton' | 'dropDownState'>
       }) => {
     const [visibleState, setVisibleState] = useState<boolean>(false);
 
@@ -27,12 +36,14 @@ const AdvancedDropdown =
     const props: T = wrapperProps === undefined ?
       {
         DropDownButton: <ButtonGenerator onClick={() => setVisibleState(!visibleState)} />,
-        displayText: displayString
+        displayText: displayString,
+        dropDownState: visibleState
       } as T :
       {
         ...wrapperProps,
         DropDownButton: <ButtonGenerator onClick={() => setVisibleState(!visibleState)} />,
-        displayText: displayString
+        displayText: displayString,
+        dropDownState: visibleState
       } as T;
 
     return (
